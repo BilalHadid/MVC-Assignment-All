9bf6f439c8747fa9053fd31f0e2e21adc8474cf2
Muhammad Bilal <hadid0648@gmail.com>
2020-04-05 01:31:14 +0500

    Website

---
+++ Task3/Task3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee emp = new Employee();
+            emp.emp_id = 1037;
+            emp.emp_name = "Sir Affan";
+            emp.emp_department = "DBMS";
+            ViewBag.Employee = emp;
+
+            studend std = new studend();
+            std.std_id = 9867;
+            std.std_name = "Bilal";
+            std.std_department = "CoCIS";
+            ViewBag.studend = std;
+            return View();
+        }
+    }
+    public class Employee
+    {
+        public int emp_id { get; set; }
+        public string emp_name { get; set; }
+        public string emp_department { get; set; }
+
+    }
+    public class studend
+    {
+        public int std_id { get; set; }
+        public string std_name { get; set; }
+        public string std_department { get; set; }
+
+    }
+}
