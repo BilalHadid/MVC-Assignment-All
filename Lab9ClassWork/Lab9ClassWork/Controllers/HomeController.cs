ce8590a412693b6173715b11ffa4763093111938
Muhammad Bilal <hadid0648@gmail.com>
2020-04-05 01:04:41 +0500

    Class work

---
+++ Lab9ClassWork/Lab9ClassWork/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab9ClassWork.Models;
+
+namespace Lab9ClassWork.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(First emp)
+        {
+            if (!string.IsNullOrEmpty(emp.UserName))
+            {
+                return RedirectToAction("Message");
+            }
+            else
+            {
+                ModelState.AddModelError("UserName", "Field Required.......");
+                return View();
+            }
+        }
+        public ActionResult Message()
+        {
+            return View();
+        }
+    }
+}
