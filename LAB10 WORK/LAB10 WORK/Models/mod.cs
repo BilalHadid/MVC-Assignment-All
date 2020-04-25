d9a32461e1fdd4b2fb0ce78ca3b450807f4510f6
Muhammad Bilal <hadid0648@gmail.com>
2020-04-25 23:23:38 +0500

    LAb Task

---
+++ LAB10 WORK/LAB10 WORK/Models/mod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB10_WORK.Models
+{
+    public class mod
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
